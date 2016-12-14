d486bf312f5465fa788563d1b34c18e8b5002e6c
nagae-memooff <169192@gmail.com>
2016-12-14 19:18:00 +0800

    添加锁屏服务器

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 	"strconv"
 	//   "strings"
 	"time"
+
+	"bufio"
+	"net"
+	"os/exec"
+	"strings"
 )
 
 const (
@@ -21,10 +26,13 @@ const (
 var (
 	Log            log.Logger
 	LogLevel       log.Level
-	version        = "nagamemofan v0.0.1"
-	contro_mode    = AUTO
+	version        = "nagamemofan v0.0.2"
+	contro_mode    = read_mode()
 	monit_interval time.Duration
 	sensors        = make(map[string]string)
+	n              int
+	listen_addr    string
+	remote_addr    string
 )
 
 func init() {
@@ -52,17 +60,49 @@ func init() {
 	} else {
 		fmt.Printf("print log to stdout.\n")
 	}
-	monit_interval = time.Duration(config.GetInt("monit_interval")) * time.Second
+
+	listen_addr = config.Get("listen_addr")
+	if listen_addr == "" {
+		listen_addr = "0.0.0.0:8899"
+	}
+
 }
 
 func main() {
+	args := os.Args
+	if len(args) == 3 && args[1] == "-c" {
+		cmd := []byte(args[2])
+		remote_addr = config.Get("remote_addr")
+		if remote_addr == "" {
+			remote_addr = "192.168.1.11:8899"
+		}
+
+		conn, err := net.Dial("tcp", remote_addr)
+		if err != nil {
+			fmt.Printf("连接服务端失败: %s", err.Error())
+			return
+		}
+		defer conn.Close()
+		conn.Write(cmd)
+
+		return
+	}
+
+	monit_interval = time.Duration(config.GetInt("monit_interval")) * time.Second
+
 	// 最简单的逻辑： 如果温度大于75度，且mode=自动，则风扇全开;
 	// 如果温度小于65度且mode=全开，则恢复自动控制
+	Log.Info("启动风扇。")
+
+	go listenAndServe()
+
 	for {
 		now_temp := read_temp()
-		Log.Info("现在温度： %d °C", now_temp/1000)
+		contro_mode = read_mode()
 
-		if now_temp > 75000 && contro_mode == AUTO {
+		Log.Info("当前温度： %d °C, 当前模式： %s", now_temp, mode_string())
+
+		if now_temp > 75 && contro_mode == AUTO {
 			Log.Info("温度高于临界，全转速。")
 			contro_mode = FULL_SPEED
 			_, err := set_mode(FULL_SPEED)
@@ -70,14 +110,20 @@ func main() {
 				Log.Error("修改模式出错：%s", err.Error())
 
 			}
-		} else if now_temp < 65000 && contro_mode == FULL_SPEED {
-			Log.Info("温度低于临界，自动控制转速。")
-			contro_mode = AUTO
-			_, err := set_mode(AUTO)
-			if err != nil {
-				Log.Error("修改模式出错：%s", err.Error())
+		} else if now_temp < 65 && contro_mode == FULL_SPEED {
+			n++
+			Log.Info("温度低于临界值 %d 次，继续观察。", n)
+			if n > 4 {
+				Log.Info("温度连续低于临界值 %d 次，自动控制转速。", n)
+				contro_mode = AUTO
+				_, err := set_mode(AUTO)
+				if err != nil {
+					Log.Error("修改模式出错：%s", err.Error())
 
+				}
 			}
+		} else {
+			n = 0
 		}
 		time.Sleep(monit_interval)
 	}
@@ -87,8 +133,28 @@ func main() {
 // 风扇转速等级： 0～255
 // 温度： 25 ～ 75
 
-func read_mode() string {
-	return read_file(config.Get("mode_controller"))
+func read_mode() (mode int) {
+	mode, err := strconv.Atoi(read_file(config.Get("mode_controller"), 1))
+	if err != nil {
+		Log.Error("读取当前控制模式失败： %s", err.Error())
+		return AUTO
+	}
+	return
+}
+
+func mode_string() (mode string) {
+	switch contro_mode {
+	case 0:
+		mode = "全速"
+	case 1:
+		mode = "手动"
+	case 2:
+		mode = "自动"
+	default:
+		mode = "未知"
+	}
+
+	return
 }
 
 func set_mode(mode int) (n int, err error) {
@@ -102,11 +168,13 @@ func set_mode(mode int) (n int, err error) {
 
 func read_temp() (temp int) {
 	for _, sensor := range sensors {
-		this_temp, err := strconv.Atoi(read_file(sensor))
+		this_temp, err := strconv.Atoi(read_file(sensor, 5))
 		if err != nil {
-			Log.Error("读取温度信息失败！ 传感器路径： %s, 值: %s, err: %s", sensor, read_file(sensor), err.Error())
+			Log.Error("读取温度信息失败！ 传感器路径： %s, 值: %s, err: %s", sensor, read_file(sensor, 5), err.Error())
 			continue
 		}
+
+		this_temp /= 1000
 		if this_temp > temp {
 			temp = this_temp
 		}
@@ -123,7 +191,7 @@ func set_speed(speed int) {
 	//TODO
 }
 
-func read_file(filename string) (out string) {
+func read_file(filename string, n int) (out string) {
 
 	_, err := os.Stat(filename)
 	if err != nil {
@@ -136,10 +204,61 @@ func read_file(filename string) (out string) {
 		return
 	}
 
-	buff := make([]byte, 5)
+	buff := make([]byte, n)
 	f.Read(buff)
 
 	return string(buff)
+}
+
+func handleConn(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		return
+	}
+
+	cmd := string(line)
+
+	switch cmd {
+	case "lock":
+		sysexec("DISPLAY=:0 gnome-screensaver-command -l")
+		Log.Info("收到指令: lock")
+	case "unlock":
+		sysexec("DISPLAY=:0 gnome-screensaver-command --exit")
+		Log.Info("收到指令: unlock")
+	default:
+		Log.Error("不支持的指令： %s ", cmd)
+		return
+	}
+
+	conn.Close()
+}
+
+func listenAndServe() {
+	ln, err := net.Listen("tcp", listen_addr)
+	if err != nil {
+		Log.Error("listen error: %s", err)
+		return
+	}
+	Log.Info("启动锁屏服务端口")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			Log.Error("accept error: %s", err)
+			break
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func sysexec(cmd string, args ...string) (result string, err error) {
+	arg := append([]string{cmd}, args...)
+	arg_str := fmt.Sprintf("%s", strings.Join(arg, " "))
+
+	ori_output, err := exec.Command("/bin/bash", "-l", "-c", arg_str).CombinedOutput()
+	return string(ori_output), err
 }
 
 // 36  2000
